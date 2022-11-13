75f174325c7e9a88037567025a65a4ccb2af30c3
Muhammad Ahsan <ahsanazeem003@gmail.com>
2022-11-14 00:22:24 +0400

    new code ex3.3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -0,0 +1,4 @@
+favourite_transportation = ['car' , " motorcycle"]
+
+print("I go for study using {}". format(favourite_transportation[0]))
+print("I for long drive using{}". format(favourite_transportation[1]))
