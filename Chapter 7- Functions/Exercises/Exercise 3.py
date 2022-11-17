72b6b86768a972f2b94bd3ecada596551f0e7f16
Muhammad Ahsan <ahsanazeem003@gmail.com>
2022-11-18 00:17:04 +0400

    new code e x7.3.py

---
+++ Chapter 7- Functions/Exercises/Exercise 3.py
@@ -0,0 +1,7 @@
+def make_shirt(size,message):
+    """Summary of how the shirt is going to be made."""
+    print(f"\nI'm going to make a {size} t-shirt")
+    print(f'It will say, "{message}"')
+
+make_shirt('medium', 'Living the good life!')
+make_shirt(message="readability counts.", size='medium')
